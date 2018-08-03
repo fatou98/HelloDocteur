8329b8032ba1bab2adda7a49f5fb094bbaf4a572
fatou98 <mbathiefatou601@gmail.com>
2018-08-03 12:18:18 +0000

    Changement sur la page daccueil recherche et detail medecin

--- HelloDocteur/src/Repository/MedecinRepository.php
+++ HelloDocteur/src/Repository/MedecinRepository.php
@@ -22,19 +22,17 @@ class MedecinRepository extends ServiceEntityRepository
 //    /**
 //     * @return Medecin[] Returns an array of Medecin objects
 //     */
-    /*
-    public function findByExampleField($value)
+    
+    public function findBySpecialite($value)
     {
         return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
+            ->andWhere('m.specialite.libelle = :val')
             ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?Medecin
